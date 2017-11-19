f35a919fafbe9f5a7562d7e7cb64005ce28917ae
GSargsyan <g.sargsyan1995@gmail.com>
2017-11-19 17:04:35 +0400

    Update commands.py

--- project/lib/commands.py
+++ project/lib/commands.py
@@ -1,27 +1,46 @@
 COMMANDS = {
-        'select': ('SELECT', 'VIEW', 'EXHIBIT', 'SHOW', 'GET', 'BRING'),
-        'create': ('CREATE', 'INSERT'),
-        'delete': ('REMOVE', 'DROP', 'DELETE'),
-        'update': ('UPDATE', 'CHANGE', 'ALTER')
+        'select': ('SELECT', 'VIEW', 'SHOW', 'GET', 'BRING', 'DISPLAY', ),
+        'create': ('CREATE', 'INSERT', 'MAKE', 'BUILD', 'INITIATE'),
+        'delete': ('REMOVE', 'DROP', 'DELETE', 'ELIMINATE', 'CUT'),
+        'update': ('UPDATE', 'CHANGE', 'ALTER', 'MODIFY', 'ADJUST', 'ALTERNATE', 'REMAKE')
         }
 
 NONOBJECTIVE = (
-        'select'
+        'select',
         )
 
 TARGETS = (
         'database',
         'db',
-        'table'
+        'table',
         'index',
         )
 
 FIELDS = {
-        'all': ('ALL', 'EVERY')
+        'all': ('ALL', 'EVERY', 'SOME', 'THOSE'),
+        'one': ('ONE', 'JUST ONE', 'SINGLE', 'THE ONLY'),
+        'max': ('MAX', 'MAXIMUM', 'HIGHEST', 'MOST', 'GREATEST'),
+        'min': ('MIN', 'MINIMUM', 'LOWEST', 'LEAST'),
+        'avg': ('AVG', 'AVERAGE'),
+        'sum': ('SUM'),
         }
 
 FUNCTION_CLASSES = {
         'create_database': 'Databases',
         'create_db': 'Databases',
         'create_table': 'Tables'
+        }
+
+CONDITION_PRONOUNS = (
+        'WHO',
+        'WHICH',
+        'WHOSE',
+        'WITH',
+        'THAT'
+        )
+
+FIELD_REFERENCES = {
+        'general': ('IS', 'ARE', 'HAS', 'HAD', 'HAVE'),
+        'name': ('CALLED', 'NAMED', 'IS', 'ARE', 'WAS', 'WERE'),
+        'location': ('LOCATED', 'FROM', 'IN', 'ON', 'AT'),
         }
